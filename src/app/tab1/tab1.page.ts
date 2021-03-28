9f2a0004ce5e4699d9bd7123fd255da57a732f95
Putu Angga Risky Raharja <anggariskyraharja@gmail.com>
2021-03-28 23:11:20 +0700

    last

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
+import { ɵDomAdapter } from '@angular/common';
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,110 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  score: number = 0;
+  classification1: number = 0.2;
+  classification2: number = 0.3;
+  q1: string = 'q11';
+  q2: string = 'q12';
+  q3: string = 'q12';
+  q4: string = 'q12';
+  q5: string = 'q12';
+  q6: string = 'q12';
+  q7: string = 'q12';
 
-  constructor() {}
+  scoring(){
+    if (this.q1 == 'q12'){
+      this.score = this.score + 1;
+    } else if (this.q1 == 'q13'){
+      this.score = this.score + 2;
+    }
+    if (this.q2 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q3 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q4 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q5 == 'q11'){
+      this.score = this.score + 2;
+    }
+    if (this.q6 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q7 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.score == 0){
+      this.classification1 = 0.2;
+      this.classification2 = 0.3;
+    } else if (this.score == 1){
+      this.classification1 = 0.6;
+      this.classification2 = 0.9;
+    } else if (this.score == 2){
+      this.classification1 = 2.2;
+      this.classification2 = 2.9;
+    } else if (this.score == 3){
+      this.classification1 = 3.2;
+      this.classification2 = 4.6;
+    } else if (this.score == 4){
+      this.classification1 = 4.8;
+      this.classification2 = 6.7;
+    } else if (this.score == 5){
+      this.classification1 = 7.2;
+      this.classification2 = 10.0;
+    } else if (this.score == 6){
+      this.classification1 = 9.7;
+      this.classification2 = 13.6;
+    } else if (this.score == 7){
+      this.classification1 = 11.2;
+      this.classification2 = 15.7;
+    } else if (this.score == 8){
+      this.classification1 = 10.8;
+      this.classification2 = 15.2;
+    } else if (this.score == 9){
+      this.classification1 = 12.2;
+      this.classification2 = 17.4;
+    }
+  }
+  checkq1(event) {
+    this.q1 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq2(event) {
+    this.q2 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq3(event) {
+    this.q3 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq4(event) {
+    this.q4 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq5(event) {
+    this.q5 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq6(event) {
+    this.q6 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq7(event) {
+    this.q7 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+
+  constructor(public alertController: AlertController) {
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
+import { ɵDomAdapter } from '@angular/common';
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,97 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  score: number = 0;
+  classification1: string = 'low';
+  classification2: number = 1.9;
+  classification3: string = null;
+  q1: string = 'q12';
+  q2: string = 'q12';
+  q3: string = 'q12';
+  q4: string = 'q12';
+  q5: string = 'q12';
+  q6: string = 'q12';
+  q7: string = 'q12';
 
-  constructor() {}
+  scoring(){
+    if (this.q1 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q2 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q3 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q4 == 'q11'){
+      this.score = this.score + 1;
+    }
+    if (this.q5 == 'q11'){
+      this.score = this.score + 2;
+      this.classification3 = 'Note: While history of stroke or TIA provides 2 points, most physicians would classify these patients directly into high risk group.'
+    } else {
+      this.classification3 = null;
+    }
+    if (this.score == 0){
+      this.classification1 = 'low';
+      this.classification2 = 1.9;
+    } else if (this.score == 1){
+      this.classification1 = 'intermediate';
+      this.classification2 = 2.8;
+    } else if (this.score == 2){
+      this.classification1 = 'intermediate';
+      this.classification2 = 4.0;
+    } else if (this.score == 3){
+      this.classification1 = 'high';
+      this.classification2 = 5.9;
+    } else if (this.score == 4){
+      this.classification1 = 'high';
+      this.classification2 = 8.5;
+    } else if (this.score == 5){
+      this.classification1 = 'high';
+      this.classification2 = 12.5;
+    } else if (this.score == 6){
+      this.classification1 = 'high';
+      this.classification2 = 18.2;
+    }
+  }
+  checkq1(event) {
+    this.q1 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq2(event) {
+    this.q2 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq3(event) {
+    this.q3 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq4(event) {
+    this.q4 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq5(event) {
+    this.q5 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq6(event) {
+    this.q6 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+  checkq7(event) {
+    this.q7 = event.target.value;
+    this.score = 0;
+    this.scoring();
+  }
+
+  constructor(public alertController: AlertController) {
+  }
 
 }
